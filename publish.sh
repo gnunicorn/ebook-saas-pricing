8415deb3918d9ec914fc3f79a49032308f3c7b09
Benjamin Kampmann <ben@gnunicorn.org>
2017-01-17 17:36:47 -0300

    cleaning up a bit

--- publish.sh
+++
@@ -1,9 +0,0 @@
-set -e
-git co master
-/Users/benjaminkampmann/dev/git-scribe/bin/git-scribe gen
-git co gh-pages
-git rm .
-mv -f output/* .
-git add --all .
-git ci -m"Update to latest master"
-git push
